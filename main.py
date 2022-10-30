a6453bfbd33afb40d99dfd874599679e78e04f08
ksm0076 <ksm4192@naver.com>
2022-10-30 17:23:39 +0900

    move select operation menu position

--- main.py
+++ main.py
@@ -19,14 +19,15 @@ logger.addHandler(file_handler)
 
 logger.debug("--PROGRAM START--")
 
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide") 
 
 
 while True:
+    print("--Select operation--")
+    print("1.Add")
+    print("2.Subtract")
+    print("3.Multiply")
+    print("4.Divide")
+    
     # take input from the user
     choice = input("Enter choice(1/2/3/4): ")
 
